ff97613471a67abda967b39f0b28a2e1798aca88
Forestieur <Forestieur@gmail.com>
2024-06-07 06:55:44 +0300

    Add namespace to DbUpdate.Program, change order of usings

--- Streetcode/DbUpdate/Program.cs
+++ Streetcode/DbUpdate/Program.cs
@@ -1,54 +1,57 @@
-﻿using DbUp;
-using Microsoft.Extensions.Configuration;
-
-public class Program
+﻿namespace Streetcode.DbUpdate
 {
-    static int Main(string[] args)
+    using DbUp;
+    using Microsoft.Extensions.Configuration;
+
+    public class Program
     {
-        string migrationPath = Path.Combine(Directory.GetCurrentDirectory(),
-            "Streetcode.DAL", "Persistence", "ScriptsMigration");
+        private static int Main(string[] args)
+        {
+            string migrationPath = Path.Combine(Directory.GetCurrentDirectory(),
+                "Streetcode.DAL", "Persistence", "ScriptsMigration");
 
-        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Local";
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Local";
 
-        var configuration = new ConfigurationBuilder()
-            .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "Streetcode.WebApi"))
-            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
-            .AddEnvironmentVariables("STREETCODE_")
-            .Build();
-
-        var connectionString = configuration.GetConnectionString("DefaultConnection");
-
-        string pathToScript = "";
-
-        Console.WriteLine("Enter '-m' to MIGRATE or '-s' to SEED db:");
-        pathToScript = Console.ReadLine();
-
-        pathToScript = migrationPath;
-        
-        var upgrader =
-            DeployChanges.To
-                .SqlDatabase(connectionString)
-                .WithScriptsFromFileSystem(pathToScript)
-                .LogToConsole()
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "Streetcode.WebApi"))
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables("STREETCODE_")
                 .Build();
 
-        var result = upgrader.PerformUpgrade();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
 
-        if (!result.Successful)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(result.Error);
-            Console.ResetColor();
+            string pathToScript = "";
+
+            Console.WriteLine("Enter '-m' to MIGRATE or '-s' to SEED db:");
+            pathToScript = Console.ReadLine();
+
+            pathToScript = migrationPath;
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsFromFileSystem(pathToScript)
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
 #if DEBUG
-            Console.ReadLine();
+                Console.ReadLine();
 #endif
-            return -1;
-        }
+                return -1;
+            }
 
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("Success!");
-        Console.ResetColor();
-        return 0;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
+        }
     }
 }
