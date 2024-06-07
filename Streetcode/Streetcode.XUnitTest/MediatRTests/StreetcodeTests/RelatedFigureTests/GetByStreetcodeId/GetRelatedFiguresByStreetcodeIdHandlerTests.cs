163d44452d7e1657e1f9c732b6a1f191b3d013f0
Ірина Наконешнюк <nakonirina1996@gmail.com>
2024-06-07 11:19:52 +0300

    test/SSAD-0008: removed_tests_for_private_methods

--- Streetcode/Streetcode.XUnitTest/MediatRTests/StreetcodeTests/RelatedFigureTests/GetByStreetcodeId/GetRelatedFiguresByStreetcodeIdHandlerTests.cs
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/StreetcodeTests/RelatedFigureTests/GetByStreetcodeId/GetRelatedFiguresByStreetcodeIdHandlerTests.cs
@@ -1,26 +1,22 @@
-﻿namespace Streetcode.XUnitTest.MediatRTests.Streetcode.RelatedFigure.GetByStreetcodeId;
+﻿namespace Streetcode.XUnitTest.MediatRTests.StreetcodeTsts.RelatedFigureTests.GetByStreetcodeId;
 
 using Moq;
 using Xunit;
 using AutoMapper;
-using System.Reflection;
 using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore.Query;
-using AutoMapper.Internal;
 using System;
 using System.Linq;
-using global::Streetcode.DAL.Repositories.Interfaces.Base;
-using global::Streetcode.BLL.Interfaces.Logging;
-using global::Streetcode.BLL.MediatR.Streetcode.RelatedFigure.GetByStreetcodeId;
-using global::Streetcode.DAL.Repositories.Interfaces.Streetcode;
-using global::Streetcode.BLL.DTO.Streetcode.RelatedFigure;
-using global::Streetcode.DAL.Entities.Streetcode;
-using global::Streetcode.DAL.Enums;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.BLL.MediatR.Streetcode.RelatedFigure.GetByStreetcodeId;
+using Streetcode.BLL.DTO.Streetcode.RelatedFigure;
+using Streetcode.DAL.Entities.Streetcode;
+using Streetcode.DAL.Enums;
 public class GetRelatedFiguresByStreetcodeIdHandlerTests
 {
     private readonly Mock<IMapper> mapperMock;
     private readonly Mock<IRepositoryWrapper> repositoryWrapperMock;
-    private readonly Mock<IRelatedFigureRepository> relatedFigureRepositoryMock = new Mock<IRelatedFigureRepository>();
     private readonly Mock<ILoggerService> loggerMock;
     private readonly GetRelatedFiguresByStreetcodeIdHandler handler;
 
@@ -32,42 +28,6 @@ public class GetRelatedFiguresByStreetcodeIdHandlerTests
         this.handler = new GetRelatedFiguresByStreetcodeIdHandler(this.mapperMock.Object, this.repositoryWrapperMock.Object, this.loggerMock.Object);
     }
 
-    [Fact]
-    public void GetRelatedFigureIdsByStreetcodeId_ShouldReturnNull_WhenArgumentNullExceptionIsThrown()
-    {
-        var streetcodeId = 1;
-        this.relatedFigureRepositoryMock.Setup(r => r.FindAll(It.IsAny<Expression<Func<RelatedFigure, bool>>>()))
-                                      .Throws<ArgumentNullException>();
-
-        this.repositoryWrapperMock.Setup(r => r.RelatedFigureRepository).Returns(this.relatedFigureRepositoryMock.Object);
-
-        var method = this.handler.GetType().GetMethod("GetRelatedFigureIdsByStreetcodeId", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
-        var result = method?.Invoke(this.handler, new object[] { streetcodeId });
-
-        Assert.Null(result);
-    }
-
-    [Fact]
-    public void GetRelatedFigureIdsByStreetcodeId_ShouldReturnIQueryable_WhenIdsAreFound()
-    {
-        var streetcodeId = 1;
-        var observerIds = new List<int> { 1, 2, 3, 5 };
-        var targetIds = new List<int> { 4, 5, 6, 1 };
-        var expectedRes = observerIds.Concat(targetIds).AsQueryable().Distinct();
-
-        this.relatedFigureRepositoryMock.SetupSequence(r => r.FindAll(It.IsAny<Expression<Func<RelatedFigure, bool>>>()))
-                                   .Returns(observerIds.Select(id => new RelatedFigure() { ObserverId = id }).Select(id => id).AsQueryable())
-                                   .Returns(targetIds.Select(id => new RelatedFigure() { TargetId = id }).Select(id => id).AsQueryable());
-
-        this.repositoryWrapperMock.Setup(r => r.RelatedFigureRepository).Returns(this.relatedFigureRepositoryMock.Object);
-
-        var method = this.handler.GetType().GetMethod("GetRelatedFigureIdsByStreetcodeId", BindingFlags.NonPublic | BindingFlags.Instance);
-        var result = method?.Invoke(this.handler, new object[] { streetcodeId }) as IQueryable<int>;
-
-        Assert.NotNull(result);
-        Assert.Equal(6, result.Count());
-        Assert.Equal(expectedRes, result);
-    }
 
     [Fact]
     public async Task Handle_ShouldReturnSuccessResult_WhenRelatedFiguresAreFound()
