e436d25eed0bd1db81570dfa6082e17e4cf78188
Ірина Наконешнюк <nakonirina1996@gmail.com>
2024-06-10 22:38:02 +0300

    test/8: moved setup mock at constructor

--- Streetcode/Streetcode.XUnitTest/MediatRTests/Streetcode/RelatedFigure/GetByStreetcodeId/GetRelatedFiguresByStreetcodeIdHandlerTests.cs
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/Streetcode/RelatedFigure/GetByStreetcodeId/GetRelatedFiguresByStreetcodeIdHandlerTests.cs
@@ -1,7 +1,7 @@
 ﻿using Moq;
 using Xunit;
-using AutoMapper;
 using System.Linq.Expressions;
+using AutoMapper;
 using Microsoft.EntityFrameworkCore.Query;
 using Streetcode.DAL.Repositories.Interfaces.Base;
 using Streetcode.BLL.Interfaces.Logging;
@@ -25,6 +25,9 @@ public class GetRelatedFiguresByStreetcodeIdHandlerTests
         repositoryWrapperMock = new Mock<IRepositoryWrapper>();
         loggerMock = new Mock<ILoggerService>();
         handler = new GetRelatedFiguresByStreetcodeIdHandler(mapperMock.Object, repositoryWrapperMock.Object, loggerMock.Object);
+
+        mapperMock.Setup(m => m.Map<IEnumerable<RelatedFigureDTO>>(It.IsAny<IEnumerable<StreetcodeContent>>()))
+          .Returns(It.IsAny<List<RelatedFigureDTO>>());
     }
 
     [Fact]
@@ -37,9 +40,6 @@ public class GetRelatedFiguresByStreetcodeIdHandlerTests
             new StreetcodeContent { Id = 2, Status = StreetcodeStatus.Published, Images = new List<DAL.Entities.Media.Images.Image>() },
             new StreetcodeContent { Id = 3, Status = StreetcodeStatus.Published, Images = new List<DAL.Entities.Media.Images.Image>() },
         };
-
-        mapperMock.Setup(m => m.Map<IEnumerable<RelatedFigureDTO>>(It.IsAny<IEnumerable<StreetcodeContent>>()))
-                  .Returns(It.IsAny<List<RelatedFigureDTO>>());
 
         repositoryWrapperMock.Setup(r => r.RelatedFigureRepository.FindAll(It.IsAny<Expression<Func<RelatedFigure, bool>>>()))
                              .Returns(new List<RelatedFigure>
@@ -63,9 +63,6 @@ public class GetRelatedFiguresByStreetcodeIdHandlerTests
     {
         var streetcodeId = 1;
         var request = new GetRelatedFigureByStreetcodeIdQuery(streetcodeId);
-
-        mapperMock.Setup(m => m.Map<IEnumerable<RelatedFigureDTO>>(It.IsAny<IEnumerable<StreetcodeContent>>()))
-                  .Returns(It.IsAny<List<RelatedFigureDTO>>());
 
         repositoryWrapperMock.Setup(r => r.RelatedFigureRepository.FindAll(It.IsAny<Expression<Func<RelatedFigure, bool>>>()))
             .Returns(Enumerable.Empty<RelatedFigure>().AsQueryable());
