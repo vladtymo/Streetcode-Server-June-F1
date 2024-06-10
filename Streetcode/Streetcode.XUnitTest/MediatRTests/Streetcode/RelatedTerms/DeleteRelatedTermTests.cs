273edc5c7ddab99228b10e434aa8b5b82df0a7de
Careless-boop <vityahalushka@gmail.com>
2024-06-10 15:32:26 +0200

    Removed intermediate TextContent folder.

--- Streetcode/Streetcode.XUnitTest/MediatRTests/Streetcode/RelatedTerms/DeleteRelatedTermTests.cs
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/Streetcode/RelatedTerms/DeleteRelatedTermTests.cs
@@ -10,7 +10,7 @@ using Streetcode.DAL.Repositories.Interfaces.Base;
 using System.Linq.Expressions;
 using Xunit;
 
-namespace Streetcode.XUnitTest.MediatRTests.Streetcode.TextContent.RelatedTerms
+namespace Streetcode.XUnitTest.MediatRTests.Streetcode.RelatedTerms
 {
     public class DeleteRelatedTermCommandHandlerTests
     {
@@ -20,9 +20,9 @@ namespace Streetcode.XUnitTest.MediatRTests.Streetcode.TextContent.RelatedTerms
 
         public DeleteRelatedTermCommandHandlerTests()
         {
-            this._mockRepository = new Mock<IRepositoryWrapper>();
-            this._mockMapper = new Mock<IMapper>();
-            this._mockLogger = new Mock<ILoggerService>();
+            _mockRepository = new Mock<IRepositoryWrapper>();
+            _mockMapper = new Mock<IMapper>();
+            _mockLogger = new Mock<ILoggerService>();
         }
 
         [Fact]
@@ -30,19 +30,19 @@ namespace Streetcode.XUnitTest.MediatRTests.Streetcode.TextContent.RelatedTerms
         {
             // Arrange
             string word = "test";
-            this.MockRepositorySetup(true);
+            MockRepositorySetup(true);
 
             var handler = new DeleteRelatedTermHandler(
-                this._mockRepository.Object,
-                this._mockMapper.Object,
-                this._mockLogger.Object);
+                _mockRepository.Object,
+                _mockMapper.Object,
+                _mockLogger.Object);
 
             // Act
             var result = await handler.Handle(new DeleteRelatedTermCommand(word), CancellationToken.None);
 
             // Assert
             Assert.True(result.HasError(e => e.Message == $"Cannot find a related term: {word}"));
-            this._mockLogger.Verify(x => x.LogError(It.IsAny<object>(), $"Cannot find a related term: {word}"), Times.Once);
+            _mockLogger.Verify(x => x.LogError(It.IsAny<object>(), $"Cannot find a related term: {word}"), Times.Once);
         }
 
         [Fact]
@@ -50,23 +50,23 @@ namespace Streetcode.XUnitTest.MediatRTests.Streetcode.TextContent.RelatedTerms
         {
             // Arrange
             string word = "test";
-            this.MockRepositorySetup(false, word);
-            this._mockRepository.Setup(x => x.SaveChangesAsync()).ReturnsAsync(1);
-            this.MockMapperSetup(false, word);
+            MockRepositorySetup(false, word);
+            _mockRepository.Setup(x => x.SaveChangesAsync()).ReturnsAsync(1);
+            MockMapperSetup(false, word);
 
             var handler = new DeleteRelatedTermHandler(
-                this._mockRepository.Object,
-                this._mockMapper.Object,
-                this._mockLogger.Object);
+                _mockRepository.Object,
+                _mockMapper.Object,
+                _mockLogger.Object);
 
             // Act
             var result = await handler.Handle(new DeleteRelatedTermCommand(word), CancellationToken.None);
 
             // Assert
             Assert.True(result.IsSuccess);
-            this._mockRepository.Verify(x => x.RelatedTermRepository.Delete(It.IsAny<RelatedTerm>()), Times.Once);
-            this._mockMapper.Verify(x => x.Map<RelatedTermDTO>(It.IsAny<RelatedTerm>()), Times.Once);
-            this._mockRepository.Verify(x => x.SaveChangesAsync(), Times.Once);
+            _mockRepository.Verify(x => x.RelatedTermRepository.Delete(It.IsAny<RelatedTerm>()), Times.Once);
+            _mockMapper.Verify(x => x.Map<RelatedTermDTO>(It.IsAny<RelatedTerm>()), Times.Once);
+            _mockRepository.Verify(x => x.SaveChangesAsync(), Times.Once);
         }
 
         [Fact]
@@ -74,21 +74,21 @@ namespace Streetcode.XUnitTest.MediatRTests.Streetcode.TextContent.RelatedTerms
         {
             // Arrange
             string word = "test";
-            this.MockRepositorySetup(false, word);
-            this._mockRepository.Setup(x => x.SaveChangesAsync()).ReturnsAsync(0);
-            this.MockMapperSetup(false, word);
+            MockRepositorySetup(false, word);
+            _mockRepository.Setup(x => x.SaveChangesAsync()).ReturnsAsync(0);
+            MockMapperSetup(false, word);
 
             var handler = new DeleteRelatedTermHandler(
-                this._mockRepository.Object,
-                this._mockMapper.Object,
-                this._mockLogger.Object);
+                _mockRepository.Object,
+                _mockMapper.Object,
+                _mockLogger.Object);
 
             // Act
             var result = await handler.Handle(new DeleteRelatedTermCommand(word), CancellationToken.None);
 
             // Assert
             Assert.True(result.HasError(e => e.Message == "Failed to delete a related term"));
-            this._mockLogger.Verify(x => x.LogError(It.IsAny<object>(), "Failed to delete a related term"), Times.Once);
+            _mockLogger.Verify(x => x.LogError(It.IsAny<object>(), "Failed to delete a related term"), Times.Once);
         }
 
         [Fact]
@@ -97,35 +97,35 @@ namespace Streetcode.XUnitTest.MediatRTests.Streetcode.TextContent.RelatedTerms
             // Arrange
             string word = "test";
 
-            this.MockRepositorySetup(false, word);
-            this._mockRepository.Setup(x => x.SaveChangesAsync()).ReturnsAsync(1);
-            this.MockMapperSetup(true);
+            MockRepositorySetup(false, word);
+            _mockRepository.Setup(x => x.SaveChangesAsync()).ReturnsAsync(1);
+            MockMapperSetup(true);
 
             var handler = new DeleteRelatedTermHandler(
-            this._mockRepository.Object,
-            this._mockMapper.Object,
-            this._mockLogger.Object);
+            _mockRepository.Object,
+            _mockMapper.Object,
+            _mockLogger.Object);
 
             // Act
             var result = await handler.Handle(new DeleteRelatedTermCommand(word), CancellationToken.None);
 
             // Assert
             Assert.True(result.HasError(e => e.Message == "Failed to delete a related term"));
-            this._mockLogger.Verify(x => x.LogError(It.IsAny<object>(), "Failed to delete a related term"), Times.Once);
+            _mockLogger.Verify(x => x.LogError(It.IsAny<object>(), "Failed to delete a related term"), Times.Once);
         }
 
         private void MockRepositorySetup(bool returnNull, string word = "")
         {
-            this._mockRepository.Setup(x => x.RelatedTermRepository
+            _mockRepository.Setup(x => x.RelatedTermRepository
                 .GetFirstOrDefaultAsync(
                 It.IsAny<Expression<Func<RelatedTerm, bool>>>(), null))
-                .ReturnsAsync(returnNull ? (RelatedTerm)null! : new RelatedTerm { Word = word });
+                .ReturnsAsync(returnNull ? null! : new RelatedTerm { Word = word });
         }
 
         private void MockMapperSetup(bool returnNull, string word = "")
         {
-            this._mockMapper.Setup(x => x.Map<RelatedTermDTO>(It.IsAny<RelatedTerm>()))
-                .Returns(returnNull ? (RelatedTermDTO)null! : new RelatedTermDTO { Word = word });
+            _mockMapper.Setup(x => x.Map<RelatedTermDTO>(It.IsAny<RelatedTerm>()))
+                .Returns(returnNull ? null! : new RelatedTermDTO { Word = word });
         }
     }
 
