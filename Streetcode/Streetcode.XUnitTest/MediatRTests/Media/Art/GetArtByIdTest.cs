9fdb7e001d006e9bfb8bb0e2e21164a8a69e37d0
yuliya <yuliyakiyanovska@gmail.com>
2024-06-10 19:58:32 +0300

    Changed name of file, methods. Removed .this

--- Streetcode/Streetcode.XUnitTest/MediatRTests/Media/Art/GetArtByIdTest.cs
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/Media/Art/GetArtByIdTest.cs
@@ -1,60 +1,60 @@
 ﻿namespace Streetcode.XUnitTest.MediatRTests.Media.Art;
-using System.Linq.Expressions;
 using AutoMapper;
 using Moq;
 using Streetcode.BLL.Interfaces.Logging;
 using Streetcode.BLL.MediatR.Media.Art.GetById;
 using Streetcode.DAL.Repositories.Interfaces.Base;
+using System.Linq.Expressions;
 using Xunit;
-public class GetArtByIdTest
+public class GetArtByIdHandlerTests
 {
     private readonly Mock<IRepositoryWrapper> repositoryWrapperMock;
     private readonly Mock<IMapper> mapperMock;
     private readonly Mock<ILoggerService> loggerMock;
     private readonly GetArtByIdHandler handler;
 
-    public GetArtByIdTest()
+    public GetArtByIdHandlerTests()
     {
-        this.repositoryWrapperMock = new Mock<IRepositoryWrapper>();
-        this.mapperMock = new Mock<IMapper>();
-        this.loggerMock = new Mock<ILoggerService>();
-        this.handler = new GetArtByIdHandler(this.repositoryWrapperMock.Object, this.mapperMock.Object, this.loggerMock.Object);
+        repositoryWrapperMock = new Mock<IRepositoryWrapper>();
+        mapperMock = new Mock<IMapper>();
+        loggerMock = new Mock<ILoggerService>();
+        handler = new GetArtByIdHandler(repositoryWrapperMock.Object, mapperMock.Object, loggerMock.Object);
     }
 
     [Fact]
-    public async Task Test_Handle_ArtExists_ReturnsSuccessResult()
+    public async Task Handle_Should_ReturnSuccess_WhenArtExists()
     {
         // Arrange
         var artId = 1;
         var request = new GetArtByIdQuery(artId);
         var artEntity = new DAL.Entities.Media.Images.Art { Id = artId };
 
-        this.repositoryWrapperMock.Setup(repo => repo.ArtRepository.GetFirstOrDefaultAsync(
+        repositoryWrapperMock.Setup(repo => repo.ArtRepository.GetFirstOrDefaultAsync(
                 It.IsAny<Expression<Func<DAL.Entities.Media.Images.Art, bool>>>(),
                 It.IsAny<Func<IQueryable<DAL.Entities.Media.Images.Art>, Microsoft.EntityFrameworkCore.Query.IIncludableQueryable<DAL.Entities.Media.Images.Art, object>>>()))
             .ReturnsAsync(artEntity);
 
         // Act
-        var result = await this.handler.Handle(request, CancellationToken.None);
+        var result = await handler.Handle(request, CancellationToken.None);
 
         // Assert
         Assert.True(result.IsSuccess);
     }
 
     [Fact]
-    public async Task Test_Handle_ArtDoesNotExist_ReturnsErrorResult_IsSuccessIsFalse()
+    public async Task Handle_Should_ReturnFailureResult_WhenTagNotExists()
     {
         // Arrange
         var artId = 1;
         var request = new GetArtByIdQuery(artId);
 
-        this.repositoryWrapperMock.Setup(repo => repo.ArtRepository.GetFirstOrDefaultAsync(
+        repositoryWrapperMock.Setup(repo => repo.ArtRepository.GetFirstOrDefaultAsync(
                 It.IsAny<Expression<Func<DAL.Entities.Media.Images.Art, bool>>>(),
                 It.IsAny<Func<IQueryable<DAL.Entities.Media.Images.Art>, Microsoft.EntityFrameworkCore.Query.IIncludableQueryable<DAL.Entities.Media.Images.Art, object>>>()))
             .ReturnsAsync(null as DAL.Entities.Media.Images.Art);
 
         // Act
-        var result = await this.handler.Handle(request, CancellationToken.None);
+        var result = await handler.Handle(request, CancellationToken.None);
 
         // Assert
         Assert.False(result.IsSuccess);
