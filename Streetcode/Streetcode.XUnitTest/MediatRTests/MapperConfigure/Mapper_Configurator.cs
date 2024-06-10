b548c6ad96450eeef5523c0522d58e55680a7d9f
BohdanLytvynov <blitvinov19@gmail.com>
2024-06-10 19:20:34 +0300

    usign statements fixed

--- Streetcode/Streetcode.XUnitTest/MediatRTests/MapperConfigure/Mapper_Configurator.cs
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/MapperConfigure/Mapper_Configurator.cs
@@ -1,13 +1,13 @@
-﻿using AutoMapper;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Streetcode.XUnitTest.MediatRTests.MapperConfigure
+﻿namespace Streetcode.XUnitTest.MediatRTests.MapperConfigure
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using AutoMapper;
+    using Microsoft.Extensions.DependencyInjection;
+
     public static class Mapper_Configurator
     {
         public static IMapper Create<TMapProfile>()
