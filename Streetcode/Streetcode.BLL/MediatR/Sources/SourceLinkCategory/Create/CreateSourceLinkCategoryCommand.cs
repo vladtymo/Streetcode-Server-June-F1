c35b21aa7ec7dd1b0d061fd1b79885dfbc2ab609
Dmytro Shavronskyy <dima.shavronskyi@gmail.com>
2024-06-13 12:16:52 +0300

    create category handler, controller, mapping, dto tests

---
+++ Streetcode/Streetcode.BLL/MediatR/Sources/SourceLinkCategory/Create/CreateSourceLinkCategoryCommand.cs
@@ -0,0 +1,8 @@
+﻿using FluentResults;
+using MediatR;
+using Streetcode.BLL.DTO.Sources;
+using Streetcode.BLL.DTO.Sources.Create;
+
+namespace Streetcode.BLL.MediatR.Sources.SourceLinkCategory.Create;
+
+public record CreateSourceLinkCategoryCommand(CreateSourceCategoryDTO newCategory) : IRequest<Result<SourceLinkCategoryDTO>>;
