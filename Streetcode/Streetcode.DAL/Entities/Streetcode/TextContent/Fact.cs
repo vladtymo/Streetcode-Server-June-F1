527c5f7a24a871cf632b90ff893e915c07b3855b
Ірина Наконешнюк <nakonirina1996@gmail.com>
2024-06-17 18:14:47 +0300

    feat/105: remove atrribute from model fact

--- Streetcode/Streetcode.DAL/Entities/Streetcode/TextContent/Fact.cs
+++ Streetcode/Streetcode.DAL/Entities/Streetcode/TextContent/Fact.cs
@@ -8,21 +8,19 @@ namespace Streetcode.DAL.Entities.Streetcode.TextContent;
 [Table("facts", Schema = "streetcode")]
 public class Fact
 {
-    [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
 
-    [Required]
-    [MaxLength(100)]
     public string? Title { get; set; }
 
-    [Required]
-    [MaxLength(600)]
     public string? FactContent { get; set; }
+
     public int? Position { get; set; }
 
     public int? ImageId { get; set; }
+
     public Image? Image { get; set; }
+
     public int StreetcodeId { get; set; }
+
     public StreetcodeContent? Streetcode { get; set; }
 }
