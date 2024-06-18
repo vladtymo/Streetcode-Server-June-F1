2f3e2690f6345c4457a8bdca395f833d6b86d04e
Roman Medvedev <r.medvedev@nltu.lviv.ua>
2024-06-18 15:50:17 +0200

    fixed bags

--- Streetcode/Streetcode.BLL/DTO/Timeline/TimelineItemDTO.cs
+++ Streetcode/Streetcode.BLL/DTO/Timeline/TimelineItemDTO.cs
@@ -5,8 +5,8 @@ namespace Streetcode.BLL.DTO.Timeline;
 public class TimelineItemDTO
 {
     public int Id { get; set; }
-    public string? Title { get; set; }
-    public string? Description { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
     public DateTime Date { get; set; }
     public DateViewPattern DateViewPattern { get; set; }
     public IEnumerable<HistoricalContextDTO>? HistoricalContexts { get; set; }
