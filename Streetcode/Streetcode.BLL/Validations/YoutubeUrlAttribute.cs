8104957a27afad1de20613d3cbac918f8caaab29
Serhii Yasenev <serhii.yasenev@gmail.com>
2024-06-20 11:42:18 +0300

    upd

--- Streetcode/Streetcode.BLL/Validations/YoutubeUrlAttribute.cs
+++ Streetcode/Streetcode.BLL/Validations/YoutubeUrlAttribute.cs
@@ -4,18 +4,18 @@ namespace Streetcode.BLL.Validations;
 
 public class YoutubeUrlAttribute : ValidationAttribute
 {
-    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
     {
         var url = value as string;
 
         if (string.IsNullOrEmpty(url))
         {
-            return ValidationResult.Success;
+            return ValidationResult.Success!;
         }
 
         if (Uri.IsWellFormedUriString(url, UriKind.Absolute) && (url.StartsWith("https://www.youtube.com/") || url.StartsWith("https://youtube.com/")))
         {
-            return ValidationResult.Success;
+            return ValidationResult.Success!;
         }
 
         return new ValidationResult("The video URL is not a valid URL - it should start with 'https://www.youtube.com/'");
