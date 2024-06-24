546b1055e91a6ea669ae8bf156b1e46817299f7e
Serhii Yasenev <serhii.yasenev@gmail.com>
2024-06-24 22:16:46 +0300

    148 delete text by Id (#168)
    * init
    
    * upd
    
    * add DeleteTextByIdHandlerTests
    
    * merge from dev
    
    * update

---
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Text/DeleteById/DeleteTextByIdCommand.cs
@@ -0,0 +1,7 @@
+﻿using FluentResults;
+using MediatR;
+using Streetcode.BLL.Behavior;
+
+namespace Streetcode.BLL.MediatR.Streetcode.Text.DeleteById;
+
+public record DeleteTextByIdCommand(int Id) : IValidatableRequest<Result<Unit>>;
