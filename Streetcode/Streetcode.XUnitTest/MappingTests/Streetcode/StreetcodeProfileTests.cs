77f9c906e54f0e644423bbab15cc78d70ad8de2f
Pavlo Yeremeiew <113441856+PavloIeremeiew@users.noreply.github.com>
2024-06-24 17:36:51 +0300

    tests/82: add tests for Mapper

--- Streetcode/Streetcode.XUnitTest/MappingTests/Streetcode/StreetcodeProfileTests.cs
+++ Streetcode/Streetcode.XUnitTest/MappingTests/Streetcode/StreetcodeProfileTests.cs
@@ -1,12 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using Streetcode.BLL.DTO.Streetcode;
+using Streetcode.BLL.Mapping.Streetcode;
+using Streetcode.BLL.Util;
+using Streetcode.DAL.Entities.Streetcode;
+using Streetcode.DAL.Entities.Streetcode.Types;
+using Streetcode.DAL.Enums;
+using Xunit;
 
-namespace Streetcode.XUnitTest.MappingTests.Streetcode
+namespace Streetcode.XUnitTest.MappingTests.StreetcodeBlock
 {
     public class StreetcodeProfileTests
     {
+        private readonly IMapper _mapper;
+
+        public StreetcodeProfileTests()
+        {
+            var mapperConfiguration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new StreetcodeProfile());
+            });
+
+            _mapper = mapperConfiguration.CreateMapper();
+        }
+
+        [Fact]
+        public void EventStreetcode_MapsCorrectly()
+        {
+            var dto = new CreateStreetcodeDTO
+            {
+                StreetcodeType = StreetcodeType.Event,
+            };
+
+            var mapped = _mapper.Map<StreetcodeContent>(dto);
+
+            Assert.IsType<EventStreetcode>(mapped);
+        }
+
+        [Fact]
+        public void PersonStreetcode_MapsCorrectly()
+        {
+            var dto = new CreateStreetcodeDTO
+            {
+                StreetcodeType = StreetcodeType.Person,
+                FirstName = "John",
+                LastName = "Doe",
+                Rank = "Captain"
+            };
+
+            var mapped = _mapper.Map<StreetcodeContent>(dto);
+
+            Assert.IsType<PersonStreetcode>(mapped);
+            Assert.Equal(dto.FirstName, ((PersonStreetcode)mapped).FirstName);
+            Assert.Equal(dto.LastName, ((PersonStreetcode)mapped).LastName);
+            Assert.Equal(dto.Rank, ((PersonStreetcode)mapped).Rank);
+        }
+
+        [Fact]
+        public void DateString_MapsCorrectly()
+        {
+            var dto = new CreateStreetcodeDTO
+            {
+                EventStartOrPersonBirthDate = DateTime.UtcNow,
+                EventEndOrPersonDeathDate = DateTime.UtcNow.AddDays(1)
+            };
+            string expected = DateToStringConverter.CreateDateString(
+                DateTime.UtcNow, DateTime.UtcNow.AddDays(1));
+
+            var mapped = _mapper.Map<StreetcodeContent>(dto);
+
+            Assert.Equal(expected, mapped.DateString);
+        }
     }
 }
