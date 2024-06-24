4a2916ed90ccfee85655912df2e8afe769013afc
Careless-boop <vityahalushka@gmail.com>
2024-06-24 02:27:02 +0200

    Created command and handler for DeleteArt.

---
+++ Streetcode/Streetcode.BLL/MediatR/Media/Art/Delete/DeleteArtCommand.cs
@@ -0,0 +1,8 @@
+﻿using FluentResults;
+using MediatR;
+using Streetcode.BLL.DTO.Media.Art;
+
+namespace Streetcode.BLL.MediatR.Media.Art.Delete
+{
+    public record DeleteArtCommand(ArtDTO Art) : IRequest<Result<ArtDTO>>;
+}
---
+++ Streetcode/Streetcode.BLL/MediatR/Media/Art/Delete/DeleteArtHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using FluentResults;
+using Streetcode.BLL.DTO.Media.Art;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+
+namespace Streetcode.BLL.MediatR.Media.Art.Delete
+{
+    public class DeleteArtHandler
+    {
+        private readonly IMapper _mapper;
+        private readonly IRepositoryWrapper _repositoryWrapper;
+        private readonly ILoggerService _logger;
+
+        public DeleteArtHandler(IRepositoryWrapper repositoryWrapper, IMapper mapper, ILoggerService logger)
+        {
+            _repositoryWrapper = repositoryWrapper;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<Result<IEnumerable<ArtDTO>>> Handle(DeleteArtCommand request, CancellationToken cancellationToken)
+        {
+            return Result.Ok();
+        }
+    }
+}
