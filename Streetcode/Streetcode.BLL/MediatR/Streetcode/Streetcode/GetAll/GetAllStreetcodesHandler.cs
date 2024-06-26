af2072ae45b8322e678aa2c576c3597ef4b487a7
Ірина Наконешнюк <nakonirina1996@gmail.com>
2024-06-27 00:28:50 +0300

    refactor/203: updates base repository to support combining specifications

--- Streetcode/Streetcode.BLL/MediatR/Streetcode/Streetcode/GetAll/GetAllStreetcodesHandler.cs
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Streetcode/GetAll/GetAllStreetcodesHandler.cs
@@ -1,13 +1,12 @@
-﻿using AutoMapper;
+﻿using Ardalis.Specification;
+using AutoMapper;
 using FluentResults;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
 using Streetcode.BLL.DTO.Streetcode;
 using Streetcode.BLL.Interfaces.Logging;
 using Streetcode.BLL.Resources;
 using Streetcode.BLL.Specification.Streetcode.Streetcode.GetAll;
 using Streetcode.BLL.Specification.Streetcode.Streetcode.GetByFilter;
-using Streetcode.BLL.Specification.Streetcode.Streetcode.NewFolder;
 using Streetcode.DAL.Entities.Streetcode;
 using Streetcode.DAL.Repositories.Interfaces.Base;
 
@@ -29,16 +28,10 @@ public class GetAllStreetcodesHandler : IRequestHandler<GetAllStreetcodesQuery, 
     public async Task<Result<GetAllStreetcodesResponseDTO>> Handle(GetAllStreetcodesQuery query, CancellationToken cancellationToken)
     {
         var filterRequest = query.request;
-        var streetcodes = _repositoryWrapper.StreetcodeRepository.FindAll();
         var amount = filterRequest.Amount;
         var page = filterRequest.Page;
 
-        if (streetcodes == null)
-        {
-            string errorMsg = string.Format(ErrorMessages.EntityNotFound, streetcodes);
-            _logger.LogError(filterRequest, errorMsg);
-            return Result.Fail(errorMsg);
-        }
+        List<ISpecification<StreetcodeContent>> specifications = new List<ISpecification<StreetcodeContent>>();
 
         if (amount <= 0 || page <= 0)
         {
@@ -47,16 +40,24 @@ public class GetAllStreetcodesHandler : IRequestHandler<GetAllStreetcodesQuery, 
             return Result.Fail(errorMsg);
         }
 
-        ApplyFilters(ref streetcodes, filterRequest);
+        ApplyFilters(specifications, filterRequest);
 
-        int pagesAmount = ApplyPagination(ref streetcodes, amount, page);
-        var filteredListStreetcodes = await streetcodes.ToListAsync();
+        specifications.Add(new StreetcodeApplyPaginationSpec(amount, page));
+
+        var filteredListStreetcodes = await _repositoryWrapper.StreetcodeRepository.GetAllWithSpecAsync(specifications.ToArray());
+
+        if (filteredListStreetcodes == null)
+        {
+            string errorMsg = string.Format(ErrorMessages.StreetCodeIsNull, nameof(filteredListStreetcodes));
+            _logger.LogError(filterRequest, errorMsg);
+            return Result.Fail(errorMsg);
+        }
 
         var streetcodeDtos = _mapper.Map<IEnumerable<StreetcodeDTO>>(filteredListStreetcodes);
 
         var response = new GetAllStreetcodesResponseDTO
         {
-            Pages = pagesAmount,
+            Pages = CalculateTotalPages(filteredListStreetcodes.Count(), amount),
             Streetcodes = streetcodeDtos
         };
 
@@ -64,32 +65,26 @@ public class GetAllStreetcodesHandler : IRequestHandler<GetAllStreetcodesQuery, 
     }
 
     private void ApplyFilters(
-        ref IQueryable<StreetcodeContent> streetcodes, GetAllStreetcodesRequestDTO filterRequest)
+         List<ISpecification<StreetcodeContent>> specifications, GetAllStreetcodesRequestDTO filterRequest)
     {
         if (!string.IsNullOrEmpty(filterRequest.Title))
         {
-            streetcodes = streetcodes.ApplySpecification(new StreetcodesFindWithMatchTitleSpec(filterRequest.Title));
+            specifications.Add(new StreetcodesFindWithMatchTitleSpec(filterRequest.Title));
         }
 
         if (!string.IsNullOrEmpty(filterRequest.Sort))
         {
-            streetcodes = streetcodes.ApplySpecification(new StreetcodesSortedByPropertySpec(filterRequest.Sort));
+            specifications.Add(new StreetcodesSortedByPropertySpec(filterRequest.Sort));
         }
 
         if (!string.IsNullOrEmpty(filterRequest.Filter))
         {
-            streetcodes = streetcodes.ApplySpecification(new StreetcodesFilteredByStatusSpec(filterRequest.Filter));
+            specifications.Add(new StreetcodesFilteredByStatusSpec(filterRequest.Filter));
         }
     }
 
-    private int ApplyPagination(
-        ref IQueryable<StreetcodeContent> streetcodes,
-        int amount,
-        int page)
+    private int CalculateTotalPages(int totalItems, int itemsPerPage)
     {
-        var totalPages = (int)Math.Ceiling(streetcodes.Count() / (double)amount);
-
-        streetcodes = streetcodes.ApplySpecification(new StreetcodeApplyPaginationSpec(amount, page));
-        return totalPages;
+        return (int)Math.Ceiling(totalItems / (double)itemsPerPage);
     }
 }
