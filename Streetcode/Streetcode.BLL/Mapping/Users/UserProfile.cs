741c97eae800bbe433dd4912764a3f56b4e6c810
BohdanLytvynov <blitvinov19@gmail.com>
2024-06-27 14:37:33 +0300

    New dtos for user created

--- Streetcode/Streetcode.BLL/Mapping/Users/UserProfile.cs
+++ Streetcode/Streetcode.BLL/Mapping/Users/UserProfile.cs
@@ -1,16 +1,48 @@
 ﻿using AutoMapper;
 using Streetcode.BLL.DTO.Users;
-
+using Streetcode.DAL.Entities.Users;
 
 namespace Streetcode.BLL.Mapping.Users
 {
     public class UserProfile : Profile
     {
-        //public UserProfile()
-        //{
-        //    CreateMap<User, UserLoginDTO>().ReverseMap();
-        //    CreateMap<UserDTO, UserLoginDTO>().ReverseMap();
-        //    CreateMap<User, UserDTO>().ReverseMap();
-        //}
+        public UserProfile()
+        {
+            CreateMap<UserRegisterDTO, User>()
+                .ForPath(
+                dest => dest.UserName, conf => conf.MapFrom(src => src.Login))                            
+                .ForPath(
+                dest => dest.FirstName, conf => conf.MapFrom(src => src.FirstName))
+                .ForPath(
+                dest => dest.LastName, conf => conf.MapFrom(src => src.LastName))
+                .ForPath(
+                dest => dest.BirthDate, conf => conf.MapFrom(src => src.Birthday))
+                .ForPath(
+                dest => dest.Email, conf => conf.MapFrom(src => src.Email))
+                .ForPath(
+                dest => dest.PhoneNumber, conf => conf.MapFrom(src => src.Phone))
+                .ReverseMap();
+
+            CreateMap<UserDTO, User>()
+                .ForPath(
+                dest => dest.UserName, conf => conf.MapFrom(src => src.Login))
+                .ForPath(
+                dest => dest.FirstName, conf => conf.MapFrom(src => src.FirstName))
+                .ForPath(
+                dest => dest.LastName, conf => conf.MapFrom(src => src.LastName))
+                .ForPath(
+                dest => dest.BirthDate, conf => conf.MapFrom(src => src.Birthday))
+                .ForPath(
+                dest => dest.Email, conf => conf.MapFrom(src => src.Email))
+                .ForPath(
+                dest => dest.PhoneNumber, conf => conf.MapFrom(src => src.Phone))
+                .ForPath(
+                dest => dest.EmailConfirmed, conf => conf.MapFrom(src => src.EmailConfirmed))
+                .ForPath(
+                dest => dest.PhoneNumberConfirmed, conf => conf.MapFrom(src => src.PhoneConfirmed))
+                .ForPath(
+                dest => dest.TwoFactorEnabled, conf => conf.MapFrom(src => src.TwoFactorEnabled))
+                .ReverseMap();
+        }
     }
 }
