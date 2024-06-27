fcdc2a613211cd5a87bb336fdc9e173558f0f753
Careless-boop <vityahalushka@gmail.com>
2024-06-27 02:39:52 +0200

    Added Register DTO, Command, Handler, DTOValidator(partly). Added AccountController and Register endpoint.

---
+++ Streetcode/Streetcode.WebApi/Controllers/Account/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Streetcode.BLL.DTO.Users;
+using Streetcode.BLL.MediatR.Account.Register;
+using Streetcode.BLL.MediatR.Partners.Create;
+
+namespace Streetcode.WebApi.Controllers.Account
+{
+    public class AccountController : BaseApiController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] UserRegisterDTO newUser)
+        {
+            return HandleResult(await Mediator.Send(new RegisterUserCommand(newUser)));
+        }
+    }
+}
