246161c813c6231923caf2f7b07934bfd12fa680
Roman Medvedev <r.medvedev@nltu.lviv.ua>
2024-06-28 23:39:57 +0200

    add check for null in tokenService

--- Streetcode/Streetcode.BLL/Services/Tokens/TokenService.cs
+++ Streetcode/Streetcode.BLL/Services/Tokens/TokenService.cs
@@ -4,7 +4,10 @@ using System.Security.Claims;
 using System.Text;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
+using Serilog;
 using SoftServerCinema.Security.Interfaces;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.BLL.Resources;
 using Streetcode.DAL.Entities.Users;
 
 namespace Streetcode.BLL.Services.Tokens;
@@ -13,45 +16,71 @@ public class TokenService : ITokenService
 {
     private readonly UserManager<User> _userManager;
     private readonly AccessTokenConfiguration _accessTokenConfiguration;
-    
-    public TokenService(UserManager<User> userManager, AccessTokenConfiguration accessTokenConfiguration)
+    private readonly ILoggerService _logger;
+    public TokenService(UserManager<User> userManager, AccessTokenConfiguration accessTokenConfiguration, ILoggerService logger)
     {
         _userManager = userManager;
         _accessTokenConfiguration = accessTokenConfiguration;
+        _logger = logger;
     }
     
     public async Task<string> GenerateAccessToken(User user, List<Claim> claims)
     {
+        if (user is null)
+        {
+            var errorMsg = MessageResourceContext.GetMessage(ErrorMessages.UserNotFound);
+            _logger.LogError(user!, errorMsg);
+            throw new ArgumentNullException(errorMsg);
+        }
+
+        if (!claims.Any())
+        {
+            claims = await GetUserClaimsAsync(user);
+        }
+
         DateTime expiration = DateTime.UtcNow.AddMinutes(_accessTokenConfiguration.AccessTokenExpirationMinutes);
+        SymmetricSecurityKey securityKey = new(Encoding.UTF8.GetBytes(_accessTokenConfiguration.SecretKey!));
+        SigningCredentials signingCredentials = new(securityKey, SecurityAlgorithms.HmacSha256);
 
-        SymmetricSecurityKey securityKey = new (Encoding.UTF8.GetBytes(_accessTokenConfiguration.SecretKey!));
-
-        SigningCredentials signingCredentials = new (securityKey, SecurityAlgorithms.HmacSha256);
-        JwtSecurityToken tokenGenerator = new (
+        JwtSecurityToken tokenGenerator = new(
             issuer: _accessTokenConfiguration.Issuer,
             audience: _accessTokenConfiguration.Audience,
             claims: claims,
             expires: expiration,
             signingCredentials: signingCredentials);
 
-        JwtSecurityTokenHandler jwtSecurityTokenHandler = new ();
-        var token = jwtSecurityTokenHandler.WriteToken(tokenGenerator);
+        JwtSecurityTokenHandler jwtSecurityTokenHandler = new();
+        string token = jwtSecurityTokenHandler.WriteToken(tokenGenerator);
+
         return token;
     }
 
     public async Task<List<Claim>> GetUserClaimsAsync(User user)
     {
-        var roles = await _userManager.GetRolesAsync(user);
-
-        List<Claim> claims = new ()
+        if (user is null)
         {
-            new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()), 
+            var errorMsg = MessageResourceContext.GetMessage(ErrorMessages.UserNotFound);
+            _logger.LogError(user!, errorMsg);
+            throw new ArgumentNullException(errorMsg);
+        }
+
+        var roles = await _userManager.GetRolesAsync(user);
+        if (!roles.Any())
+        {
+            var errorMsg = MessageResourceContext.GetMessage(ErrorMessages.RolesNotFound);
+            _logger.LogError(roles!, errorMsg);
+            throw new ArgumentNullException(errorMsg);
+        }
+
+        List<Claim> claims = new()
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
             new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-            new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString(CultureInfo.CurrentCulture)),
-            new Claim(ClaimTypes.NameIdentifier, user.Email), 
-            new Claim(ClaimTypes.Name, user.UserName), 
-            new Claim(ClaimTypes.Email, user.Email), 
-            new Claim(ClaimTypes.Role, roles.FirstOrDefault() !)
+            new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString(CultureInfo.InvariantCulture)),
+            new Claim(ClaimTypes.NameIdentifier, user.Email),
+            new Claim(ClaimTypes.Name, user.UserName),
+            new Claim(ClaimTypes.Email, user.Email),
+            new Claim(ClaimTypes.Role, roles.First())
         };
 
         return claims;
@@ -59,6 +88,22 @@ public class TokenService : ITokenService
 
     public ClaimsPrincipal GetPrincipalFromAccessToken(string? token)
     {
+        if (string.IsNullOrEmpty(token))
+        {
+            var errorMsg = MessageResourceContext.GetMessage(ErrorMessages.InvalidToken);
+            _logger.LogError(token!, errorMsg);
+            throw new ArgumentNullException(errorMsg);
+        }
+        
+        JwtSecurityTokenHandler tokenHandler = new();
+
+        if (!tokenHandler.CanReadToken(token))
+        {
+            var errorMsg = MessageResourceContext.GetMessage(ErrorMessages.InvalidToken);
+            _logger.LogError(token, errorMsg);
+            throw new ArgumentNullException(errorMsg);
+        }
+        
         var tokenValidationParameters = new TokenValidationParameters
         {
             ValidateAudience = true,
@@ -69,9 +114,9 @@ public class TokenService : ITokenService
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_accessTokenConfiguration.SecretKey!)),
             ValidateLifetime = true
         };
-        
-        JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+
         ClaimsPrincipal principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out SecurityToken securityToken);
+
         return principal;
     }
 }
