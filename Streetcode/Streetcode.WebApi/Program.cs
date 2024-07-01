f3d86d0673ab5ea3f011dca4bf0b16df9f249cca
BohdanLytvynov <blitvinov19@gmail.com>
2024-07-01 11:49:30 +0300

    Comment Data Seeding

--- Streetcode/Streetcode.WebApi/Program.cs
+++ Streetcode/Streetcode.WebApi/Program.cs
@@ -37,7 +37,7 @@ app.UseMiddleware<GenericExceptionHandlerMiddleware>();
 
 await app.ApplyMigrations();
 
-await app.SeedDataAsync(); // uncomment for seeding data in local
+//await app.SeedDataAsync(); // uncomment for seeding data in local
 app.UseCors();
 app.UseHttpsRedirection();
 app.UseRouting();
