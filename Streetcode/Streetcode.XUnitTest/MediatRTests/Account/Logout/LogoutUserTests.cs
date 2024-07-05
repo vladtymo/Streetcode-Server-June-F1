b5e085e2b2bac13d19f9246a4dda0bb9bf975af8
Roman Medvedev <r.medvedev@nltu.lviv.ua>
2024-07-05 16:18:58 +0200

    fix logout tests

--- Streetcode/Streetcode.XUnitTest/MediatRTests/Account/Logout/LogoutUserTests.cs
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/Account/Logout/LogoutUserTests.cs
@@ -64,15 +64,51 @@ namespace Streetcode.XUnitTest.MediatRTests.Account.Logout
         {
             // Arrange
             var command = new LogoutUserCommand();
-            var httpContext = new DefaultHttpContext();
-            httpContext.Response.Cookies.Append("accessToken", "validToken", new CookieOptions());
-            _httpContextAccessorMock.Setup(x => x.HttpContext).Returns(httpContext);
-            _tokenServiceMock.Setup(x => x.GetUserIdFromAccessToken(It.IsAny<string>())).Returns("userId");
+            var refreshTokenValue = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64));
+            var refreshToken = new List<DAL.Entities.Users.RefreshToken>
+            {
+                new DAL.Entities.Users.RefreshToken
+                {
+                    Token = refreshTokenValue,
+                    Expires = DateTime.UtcNow.AddDays(2)
+                }
+            };
 
-            var user = new User { Id = Guid.NewGuid() };
+            var user = new User
+            {
+                Id = Guid.NewGuid(),
+                RefreshTokens = refreshToken
+            };
+
+            var requestCookies = new Mock<IRequestCookieCollection>();
+            requestCookies.Setup(x => x.TryGetValue("accessToken", out It.Ref<string>.IsAny!)).Returns((string key, out string value) =>
+            {
+                value = "validAccessToken";
+                return true;
+            });
+            requestCookies.Setup(x => x.TryGetValue("refreshToken", out It.Ref<string>.IsAny!)).Returns((string key, out string value) =>
+            {
+                value = refreshTokenValue;
+                return true;
+            });
+            requestCookies.Setup(x => x.Keys).Returns(new List<string> { "accessToken", "refreshToken" });
+
+            var responseCookiesMock = new Mock<IResponseCookies>();
+            var responseMock = new Mock<HttpResponse>();
+            responseMock.Setup(r => r.Cookies).Returns(responseCookiesMock.Object);
+
+            var requestMock = new Mock<HttpRequest>();
+            requestMock.Setup(r => r.Cookies).Returns(requestCookies.Object);
+
+            var httpContextMock = new Mock<HttpContext>();
+            httpContextMock.Setup(ctx => ctx.Request).Returns(requestMock.Object);
+            httpContextMock.Setup(ctx => ctx.Response).Returns(responseMock.Object);
+            _httpContextAccessorMock.Setup(x => x.HttpContext).Returns(httpContextMock.Object);
+            _tokenServiceMock.Setup(x => x.GetUserIdFromAccessToken(It.IsAny<string>())).Returns(user.Id.ToString());
+            _userManagerMock.Setup(x => x.Users).Returns(new List<User> { user }.AsQueryable().BuildMockDbSet().Object);
             _userManagerMock.Setup(x => x.FindByIdAsync(It.IsAny<string>())).ReturnsAsync(user);
             _userManagerMock.Setup(x => x.UpdateAsync(It.IsAny<User>())).ReturnsAsync(IdentityResult.Failed());
-
+            
             var errorMsg = MessageResourceContext.GetMessage(ErrorMessages.UserUpdateFailed, command);
 
             // Act
@@ -88,12 +124,48 @@ namespace Streetcode.XUnitTest.MediatRTests.Account.Logout
         {
             // Arrange
             var command = new LogoutUserCommand();
-            var httpContext = new DefaultHttpContext();
-            httpContext.Response.Cookies.Append("accessToken", "validToken", new CookieOptions());
-            _httpContextAccessorMock.Setup(x => x.HttpContext).Returns(httpContext);
-            _tokenServiceMock.Setup(x => x.GetUserIdFromAccessToken(It.IsAny<string>())).Returns("userId");
+            var refreshTokenValue = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64));
+            var refreshToken = new List<DAL.Entities.Users.RefreshToken>
+            {
+                new DAL.Entities.Users.RefreshToken
+                {
+                    Token = refreshTokenValue,
+                    Expires = DateTime.UtcNow.AddDays(2)
+                }
+            };
 
-            var user = new User { Id = Guid.NewGuid() };
+            var user = new User
+            {
+                Id = Guid.NewGuid(),
+                RefreshTokens = refreshToken
+            };
+
+            var requestCookies = new Mock<IRequestCookieCollection>();
+            requestCookies.Setup(x => x.TryGetValue("accessToken", out It.Ref<string>.IsAny!)).Returns((string key, out string value) =>
+            {
+                value = "validAccessToken";
+                return true;
+            });
+            requestCookies.Setup(x => x.TryGetValue("refreshToken", out It.Ref<string>.IsAny!)).Returns((string key, out string value) =>
+            {
+                value = refreshTokenValue;
+                return true;
+            });
+            requestCookies.Setup(x => x.Keys).Returns(new List<string> { "accessToken", "refreshToken" });
+
+            var responseCookiesMock = new Mock<IResponseCookies>();
+            var responseMock = new Mock<HttpResponse>();
+            responseMock.Setup(r => r.Cookies).Returns(responseCookiesMock.Object);
+
+            var requestMock = new Mock<HttpRequest>();
+            requestMock.Setup(r => r.Cookies).Returns(requestCookies.Object);
+
+            var httpContextMock = new Mock<HttpContext>();
+            httpContextMock.Setup(ctx => ctx.Request).Returns(requestMock.Object);
+            httpContextMock.Setup(ctx => ctx.Response).Returns(responseMock.Object);
+            _httpContextAccessorMock.Setup(x => x.HttpContext).Returns(httpContextMock.Object);
+            _tokenServiceMock.Setup(x => x.GetUserIdFromAccessToken(It.IsAny<string>())).Returns(user.Id.ToString());
+            _userManagerMock.Setup(x => x.Users).Returns(new List<User> { user }.AsQueryable().BuildMockDbSet().Object);
             _userManagerMock.Setup(x => x.FindByIdAsync(It.IsAny<string>())).ReturnsAsync(user);
             _userManagerMock.Setup(x => x.UpdateAsync(It.IsAny<User>())).ReturnsAsync(IdentityResult.Success);
             _cacheServiceMock.Setup(x => x.SetBlacklistedTokenAsync(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(false);
@@ -108,68 +180,68 @@ namespace Streetcode.XUnitTest.MediatRTests.Account.Logout
             Assert.Equal(errorMsg, result.Errors[0].Message);
         }
         
-      [Fact]
-public async Task Handle_ReturnsSuccess_WhenLogoutIsSuccessful()
-{
-    // Arrange
-    var command = new LogoutUserCommand();
-    var refreshTokenValue = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64));
-    var refreshToken = new List<DAL.Entities.Users.RefreshToken>
-    {
-        new DAL.Entities.Users.RefreshToken
+        [Fact]
+        public async Task Handle_ReturnsSuccess_WhenLogoutIsSuccessful()
         {
-            Token = refreshTokenValue,
-            Expires = DateTime.UtcNow.AddDays(2)
+            // Arrange
+            var command = new LogoutUserCommand();
+            var refreshTokenValue = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64));
+            var refreshToken = new List<DAL.Entities.Users.RefreshToken>
+            {
+                new DAL.Entities.Users.RefreshToken
+                {
+                    Token = refreshTokenValue,
+                    Expires = DateTime.UtcNow.AddDays(2)
+                }
+            };
+
+            var user = new User
+            {
+                Id = Guid.NewGuid(),
+                RefreshTokens = refreshToken
+            };
+
+            var requestCookies = new Mock<IRequestCookieCollection>();
+            requestCookies.Setup(x => x.TryGetValue("accessToken", out It.Ref<string>.IsAny!)).Returns((string key, out string value) =>
+            {
+                value = "validAccessToken";
+                return true;
+            });
+            requestCookies.Setup(x => x.TryGetValue("refreshToken", out It.Ref<string>.IsAny!)).Returns((string key, out string value) =>
+            {
+                value = refreshTokenValue;
+                return true;
+            });
+            requestCookies.Setup(x => x.Keys).Returns(new List<string> { "accessToken", "refreshToken" });
+
+            var responseCookiesMock = new Mock<IResponseCookies>();
+            var responseMock = new Mock<HttpResponse>();
+            responseMock.Setup(r => r.Cookies).Returns(responseCookiesMock.Object);
+
+            var requestMock = new Mock<HttpRequest>();
+            requestMock.Setup(r => r.Cookies).Returns(requestCookies.Object);
+
+            var httpContextMock = new Mock<HttpContext>();
+            httpContextMock.Setup(ctx => ctx.Request).Returns(requestMock.Object);
+            httpContextMock.Setup(ctx => ctx.Response).Returns(responseMock.Object);
+
+            _httpContextAccessorMock.Setup(x => x.HttpContext).Returns(httpContextMock.Object);
+            _tokenServiceMock.Setup(x => x.GetUserIdFromAccessToken(It.IsAny<string>())).Returns(user.Id.ToString());
+            _userManagerMock.Setup(x => x.Users).Returns(new List<User> { user }.AsQueryable().BuildMockDbSet().Object);
+            _userManagerMock.Setup(x => x.FindByIdAsync(It.IsAny<string>())).ReturnsAsync(user);
+            _userManagerMock.Setup(x => x.UpdateAsync(It.IsAny<User>())).ReturnsAsync(IdentityResult.Success);
+            _cacheServiceMock.Setup(x => x.SetBlacklistedTokenAsync(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(true);
+
+            // Act
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            Assert.True(result.IsSuccess);
+            Assert.Equal("User logged out successfully", result.Value);
+            responseCookiesMock.Verify(x => x.Delete(It.IsAny<string>(), It.IsAny<CookieOptions>()), Times.AtLeast(2));
+            _userManagerMock.Verify(x => x.UpdateAsync(It.Is<User>(u => u.RefreshTokens.Count == 0)), Times.Once);
+            _cacheServiceMock.Verify(x => x.SetBlacklistedTokenAsync("validAccessToken", user.Id.ToString()), Times.Once);
         }
-    };
-
-    var user = new User
-    {
-        Id = Guid.NewGuid(),
-        RefreshTokens = refreshToken
-    };
-
-    var requestCookies = new Mock<IRequestCookieCollection>();
-    requestCookies.Setup(x => x.TryGetValue("accessToken", out It.Ref<string>.IsAny!)).Returns((string key, out string value) =>
-    {
-        value = "validAccessToken";
-        return true;
-    });
-    requestCookies.Setup(x => x.TryGetValue("refreshToken", out It.Ref<string>.IsAny!)).Returns((string key, out string value) =>
-    {
-        value = refreshTokenValue;
-        return true;
-    });
-    requestCookies.Setup(x => x.Keys).Returns(new List<string> { "accessToken", "refreshToken" });
-
-    var responseCookiesMock = new Mock<IResponseCookies>();
-    var responseMock = new Mock<HttpResponse>();
-    responseMock.Setup(r => r.Cookies).Returns(responseCookiesMock.Object);
-
-    var requestMock = new Mock<HttpRequest>();
-    requestMock.Setup(r => r.Cookies).Returns(requestCookies.Object);
-
-    var httpContextMock = new Mock<HttpContext>();
-    httpContextMock.Setup(ctx => ctx.Request).Returns(requestMock.Object);
-    httpContextMock.Setup(ctx => ctx.Response).Returns(responseMock.Object);
-
-    _httpContextAccessorMock.Setup(x => x.HttpContext).Returns(httpContextMock.Object);
-    _tokenServiceMock.Setup(x => x.GetUserIdFromAccessToken(It.IsAny<string>())).Returns(user.Id.ToString());
-    _userManagerMock.Setup(x => x.Users).Returns(new List<User> { user }.AsQueryable().BuildMockDbSet().Object);
-    _userManagerMock.Setup(x => x.FindByIdAsync(It.IsAny<string>())).ReturnsAsync(user);
-    _userManagerMock.Setup(x => x.UpdateAsync(It.IsAny<User>())).ReturnsAsync(IdentityResult.Success);
-    _cacheServiceMock.Setup(x => x.SetBlacklistedTokenAsync(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(true);
-
-    // Act
-    var result = await _handler.Handle(command, CancellationToken.None);
-
-    // Assert
-    Assert.True(result.IsSuccess);
-    Assert.Equal("User logged out successfully", result.Value);
-    responseCookiesMock.Verify(x => x.Delete(It.IsAny<string>(), It.IsAny<CookieOptions>()), Times.AtLeast(2));
-    _userManagerMock.Verify(x => x.UpdateAsync(It.Is<User>(u => u.RefreshTokens.Count == 0)), Times.Once);
-    _cacheServiceMock.Verify(x => x.SetBlacklistedTokenAsync("validAccessToken", user.Id.ToString()), Times.Once);
-}
 
     }
 }
