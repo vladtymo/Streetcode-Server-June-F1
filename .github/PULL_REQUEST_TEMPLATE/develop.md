b2d878dae3428153a746b58dfbf228996bbc77c8
Forestieur <Forestieur@gmail.com>
2024-07-08 06:59:54 +0300

    added policy

---
+++ .github/PULL_REQUEST_TEMPLATE/develop.md
@@ -0,0 +1,11 @@
+dev
+## Describe your changes . Please include a summary of the changes and  also include relevant motivation and context
+
+## Issue ticket number and link
+
+## Checklist before requesting a review
+- [ ] Bug fix (non-breaking change which fixes an issue)
+- [ ] New feature (non-breaking change which adds functionality)
+- [ ] Have you lint your code locally before submission?
+- [ ] If that PR have tests
+- [ ] I have performed a self-review of my code
--- Streetcode/Streetcode.WebApi/Extensions/ServiceCollectionExtensions.cs
+++ Streetcode/Streetcode.WebApi/Extensions/ServiceCollectionExtensions.cs
@@ -30,6 +30,7 @@ using Streetcode.DAL.Entities.Users;
 using Streetcode.BLL.Services.CacheService;
 using Streetcode.BLL.Services.Tokens;
 using Streetcode.WebApi.Events;
+using Streetcode.DAL.Enums;
 
 namespace Streetcode.WebApi.Extensions;
 
@@ -111,6 +112,12 @@ public static class ServiceCollectionExtensions
                 options.RequireHttpsMetadata = false;
                 options.EventsType = typeof(JwtTokenValidationEvents);
             });
+
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy("AdminPolicy", policy => policy.RequireRole(UserRole.Admin.ToString()));
+            options.AddPolicy("UserPolicy", policy => policy.RequireRole(UserRole.User.ToString()));
+        });
         services.AddScoped<JwtTokenValidationEvents>();
     }
     
