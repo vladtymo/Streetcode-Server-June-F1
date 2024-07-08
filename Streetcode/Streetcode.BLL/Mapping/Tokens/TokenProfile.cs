1f23a4cb7e54262f655abce8112d3da3e9a4f49e
Dmytro Shavronskyy <dima.shavronskyi@gmail.com>
2024-07-08 18:48:44 +0300

    add automapper for refreshtoken refactor tokenservice

---
+++ Streetcode/Streetcode.BLL/Mapping/Tokens/TokenProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Streetcode.BLL.DTO.Users;
+using Streetcode.DAL.Entities.Users;
+
+namespace Streetcode.BLL.Mapping.Tokens
+{
+    public class TokenProfile : Profile
+    {
+        public TokenProfile()
+        {
+            CreateMap<RefreshToken, RefreshTokenDTO>()
+                .ForPath(dest => dest.Token, conf => conf.MapFrom(src => src.Token))
+                .ForPath(dest => dest.Created, conf => conf.MapFrom(src => src.Created))
+                .ForPath(dest => dest.Expires, conf => conf.MapFrom(src => src.Expires))
+                .ReverseMap();
+        }
+    }
+}
