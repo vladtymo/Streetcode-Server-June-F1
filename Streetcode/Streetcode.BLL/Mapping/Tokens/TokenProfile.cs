12408465c30bb1fc0b4db3cef9fee1cc3fb6cecc
shavronskyy <dima.shavronskyi@gmail.com>
2024-07-09 19:52:39 +0300

    updated mapper

--- Streetcode/Streetcode.BLL/Mapping/Tokens/TokenProfile.cs
+++ Streetcode/Streetcode.BLL/Mapping/Tokens/TokenProfile.cs
@@ -8,11 +8,7 @@ namespace Streetcode.BLL.Mapping.Tokens
     {
         public TokenProfile()
         {
-            CreateMap<RefreshToken, RefreshTokenDTO>()
-                .ForPath(dest => dest.Token, conf => conf.MapFrom(src => src.Token))
-                .ForPath(dest => dest.Created, conf => conf.MapFrom(src => src.Created))
-                .ForPath(dest => dest.Expires, conf => conf.MapFrom(src => src.Expires))
-                .ReverseMap();
+            CreateMap<RefreshToken, RefreshTokenDTO>().ReverseMap();
         }
     }
 }
