491595e49fb8080cb92b52a9b7dcd40c8e71af1c
Forestieur <Forestieur@gmail.com>
2024-07-11 00:12:04 +0300

    suppresed default identity requirements for password, moved requirements to validator

--- Streetcode/Streetcode.BLL/MediatR/Account/Register/RegisterUserRequestDTOValidator.cs
+++ Streetcode/Streetcode.BLL/MediatR/Account/Register/RegisterUserRequestDTOValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using Streetcode.BLL.Interfaces.Users;
 
 namespace Streetcode.BLL.MediatR.Account.Register
 {
@@ -8,6 +9,10 @@ namespace Streetcode.BLL.MediatR.Account.Register
         {
             RuleFor(u => u.newUser.Email).EmailAddress();
             RuleFor(u => u.newUser.Password).MinimumLength(7);
+            RuleFor(u => u.newUser.Password).Matches("[A-Z]");
+            RuleFor(u => u.newUser.Password).Matches("[a-z]");
+            RuleFor(u => u.newUser.Password).Matches("[0-9]");
+            RuleFor(u => u.newUser.Password).Matches("[^a-zA-Z0-9]");
             RuleFor(u => u.newUser.Username).NotEmpty().Length(4, 256);
 
             // Add more if needed
