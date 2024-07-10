af48f5ac74670c6433d242965c84e54b10b054a2
Forestieur <Forestieur@gmail.com>
2024-07-10 23:28:39 +0300

    add validation to validator

--- Streetcode/Streetcode.BLL/MediatR/Account/ChangePassword/ChangePasswordDTOValidator.cs
+++ Streetcode/Streetcode.BLL/MediatR/Account/ChangePassword/ChangePasswordDTOValidator.cs
@@ -7,8 +7,14 @@ namespace Streetcode.BLL.MediatR.Account.ChangePassword
         public ChangePasswordDTOValidator()
         {
             RuleFor(u => u.PasswordChange.NewPassword).MinimumLength(7);
+            RuleFor(u => u.PasswordChange.NewPassword).Matches("[A-Z]");
+            RuleFor(u => u.PasswordChange.NewPassword).Matches("[a-z]");
+            RuleFor(u => u.PasswordChange.NewPassword).Matches("[0-9]");
+            RuleFor(u => u.PasswordChange.NewPassword).Matches("[^a-zA-Z0-9]");
+
             RuleFor(u => u.PasswordChange).Must(x => x.NewPassword == x.ConfirmPassword);
             RuleFor(u => u.PasswordChange).Must(x => x.NewPassword != x.CurrentPassword);
+
         }
     }
 }
