51e25d8a1c9b2a071101d7ef77f2c534f5e774a3
Pavlo Yeremeiew <113441856+PavloIeremeiew@users.noreply.github.com>
2024-07-12 11:26:35 +0300

    feat/302: add BadWordsverification

---
+++ Streetcode/Streetcode.BLL/Util/Comments/BadWordsverification.cs
@@ -0,0 +1,13 @@
+﻿using mk.profanity; 
+
+namespace Streetcode.BLL.Util.Comments
+{
+    public class BadWordsverification
+    {
+        public static bool NotContainBadWords(string text)
+        {
+            ProfanityFilter profanityFilter = new ProfanityFilter();
+            return !profanityFilter.ContainsProfanity(text);
+        }
+    }
+}
